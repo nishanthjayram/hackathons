097cb42d1180e7f737061ef27df97dd55f8f3c0e
Seant001 <seantouchstonejr@gmail.com>
2021-01-16 02:10:21 -0800

    Turned the datahandler into an object with the addNewJob functiion for easier functionality

--- cruzhacks21/datahandler.py
+++ cruzhacks21/datahandler.py
@@ -9,32 +9,35 @@ import sys #only needed to determine Python version number
 import matplotlib #only needed to determine Matplotlib version number
 class datahandler:
 
-#'Company Name','Location', 'Job Title', 'Seniority Level', 'Employment Type'
-    job1 = ['Apple', 'Cupertino', 'Software Enigneer 2', 'Manager', 'Full-Time']
-    job2 = ['Tesla', 'Fremont', 'Software Enigneer 3', 'Associate', 'Full-Time']
-    job3 = ['Amazon', 'Seattle', 'Hardware Engineer 3', 'Manager', 'Full-time']
-    job4 = ['Boeing', 'Seattle', 'Hardware Engineer 1', 'Associate', 'Full-time']
-    data = [job1,job2]
+    def __init__(self, df):
+        self.df = df
 
-# Create the pandas DataFrame
-    df = pd.DataFrame(data, columns=['Company Name',
-                                       'Location', 'Job Title', 'Seniority Level', 'Employment Type'])
-# print dataframe
-    print(df)
-# Export dataframe to csv file
-    df.to_csv('jobApps.txt', index=False)
-# Export dataframe to excel file
-    df.to_excel("output.xlsx")
 
 # Add new job to new database
-    def addNewJob(job):
+    def addNewJob(self, job):
         new_df = pd.DataFrame([job], columns = ['Company Name',
                                              'Location', 'Job Title','Seniority Level', 'Employment Type'])
-        print(new_df)
-        return new_df
+        self.df = self.df.append(new_df, ignore_index=True)
 
-    df = df.append(addNewJob(job3), ignore_index=True)
-    df = df.append(addNewJob(job4), ignore_index=True)
 
-    print(df)
 
+#'Company Name','Location', 'Job Title', 'Seniority Level', 'Employment Type'
+job1 = ['Apple', 'Cupertino', 'Software Enigneer 2', 'Manager', 'Full-Time']
+job2 = ['Tesla', 'Fremont', 'Software Enigneer 3', 'Associate', 'Full-Time']
+job3 = ['Amazon', 'Seattle', 'Hardware Engineer 3', 'Manager', 'Full-time']
+job4 = ['Boeing', 'Seattle', 'Hardware Engineer 1', 'Associate', 'Full-time']
+data = [job1,job2]
+
+
+# Create the pandas DataFrame
+dataFrame = pd.DataFrame(data, columns=['Company Name',
+                                       'Location', 'Job Title', 'Seniority Level', 'Employment Type'])
+dh = datahandler(dataFrame)
+dh.addNewJob(job3)
+dh.addNewJob(job4)
+# print dataframe
+print(dh.df)
+# Export dataframe to csv file
+dataFrame.to_csv('jobApps.txt', index=False)
+# Export dataframe to excel file
+dataFrame.to_excel("output.xlsx")
