9371261a128a099c3dcb7f9816b5963339eb7774
Seant001 <seantouchstonejr@gmail.com>
2021-01-16 21:36:40 -0800

    Added simple firebase functionality for storing jobs into a database

--- cruzhacks21/datahandler.py
+++ cruzhacks21/datahandler.py
@@ -30,7 +30,7 @@ job1 = ['Apple', 'Cupertino', 'Software Enigneer 2', 'Manager', 'Full-Time']
 job2 = ['Tesla', 'Fremont', 'Software Enigneer 3', 'Associate', 'Full-Time']
 job3 = ['Amazon', 'Seattle', 'Hardware Engineer 3', 'Manager', 'Full-time']
 job4 = ['Boeing', 'Seattle', 'Hardware Engineer 1', 'Associate', 'Full-time']
-dataList = [job1,job2]
+dataList = [job1,job2,job3]
 
 # Create the pandas DataFrame
 dh = datahandler(dataList)
