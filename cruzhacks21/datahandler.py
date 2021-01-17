b4c461e0533815e666f82d0060e34e5e67e6cd62
Seant001 <seantouchstonejr@gmail.com>
2021-01-17 01:01:32 -0800

    Added additional firebase functionality and moved all firebase functions into the datahandler wrapper

--- cruzhacks21/datahandler.py
+++ cruzhacks21/datahandler.py
@@ -10,38 +10,6 @@ import matplotlib #only needed to determine Matplotlib version number
 import pyrebase
 import firebase_admin
 from firebase_admin import auth as authen
-class datahandler:
-
-    def __init__(self,data):
-        self.df = pd.DataFrame(data, columns=['Company Name',
-                                       'Location', 'Job Title', 'Seniority Level', 'Employment Type'])
-
-
-# Add new job to new database
-    def addNewJob(self, job):
-        new_df = pd.DataFrame([job], columns = ['Company Name',
-                                             'Location', 'Job Title','Seniority Level', 'Employment Type'])
-        self.df = self.df.append(new_df, ignore_index=True)
-
-
-
-#'Company Name','Location', 'Job Title', 'Seniority Level', 'Employment Type'
-job1 = ['Apple', 'Cupertino', 'Software Enigneer 2', 'Manager', 'Full-Time']
-job2 = ['Tesla', 'Fremont', 'Software Enigneer 3', 'Associate', 'Full-Time']
-job3 = ['Amazon', 'Seattle', 'Hardware Engineer 3', 'Manager', 'Full-time']
-job4 = ['Boeing', 'Seattle', 'Hardware Engineer 1', 'Associate', 'Full-time']
-dataList = [job1,job2,job3]
-
-# Create the pandas DataFrame
-dh = datahandler(dataList)
-dh.addNewJob(job3)
-# print dataframe
-print(dh.df)
-# Export dataframe to csv file
-dh.df.to_csv('jobApps.txt', index=False)
-# Export dataframe to excel file
-dh.df.to_excel("output.xlsx")
-
 config = {
   "apiKey": "AIzaSyBVQ2tMd1fIGawv-V5Yo7ZUCIFBEY4QjNw",
   "authDomain": "jojoba-internship-tracker.firebaseapp.com",
@@ -51,6 +19,44 @@ config = {
 }
 
 firebase = pyrebase.initialize_app(config)
+class datahandler:
+
+    def __init__(self,data):
+        self.df = pd.DataFrame(data, columns=['Company Name',
+                                       'Location', 'Job Title', 'Seniority Level', 'Employment Type'])
+        # Get a reference to the database service
+        self.db = firebase.database()
+
+# Add new job to new database
+    def addNewJob(self, job):
+        new_df = pd.DataFrame([job], columns = ['Company Name',
+                                             'Location', 'Job Title','Seniority Level', 'Employment Type'])
+        self.df = self.df.append(new_df, ignore_index=True)
+
+# Update and Upload the pandas dataframe to firebase
+    def updateUpload(self):
+        self.db.child("Jobs").set(self.df.values.tolist())
+
+# Export dataframe to excel file
+    def exportToExcel(self):
+        self.df.to_excel("jobApplications.xlsx")
+
+# Export dataframe to CSV file
+    def exportToExcel(self):
+        self.df.to_csv('jobApplications.txt', index=False)
+
+#'Company Name','Location', 'Job Title', 'Seniority Level', 'Employment Type' (TEST CASES)
+job1 = ['Apple', 'Cupertino', 'Software Enigneer 2', 'Manager', 'Full-Time']
+job2 = ['Tesla', 'Fremont', 'Software Enigneer 3', 'Associate', 'Full-Time']
+job3 = ['Amazon', 'Seattle', 'Hardware Engineer 3', 'Manager', 'Full-time']
+job4 = ['Boeing', 'Seattle', 'Hardware Engineer 1', 'Associate', 'Full-time']
+dataList = [job1,job2,job3]
+
+# Create the pandas DataFrame
+dh = datahandler(dataList)
+# print dataframe
+print(dh.df)
+
 
 # Get a reference to the auth service
 auth = firebase.auth()
@@ -63,15 +69,6 @@ user = auth.sign_in_with_email_and_password(email, password)
 
 # Log the user in anonymously
 #user = auth.sign_in_anonymous()
-data = {"name": "Mortimer 'Morty' Smith"}
-# Get a reference to the database service
-db = firebase.database()
-# Pass the user's idToken to the push method
-#results = db.child("users").push(job3, user['idToken'])
+dh.updateUpload()
 
-def addFirebaseJob(job):
-    db.child("users").push(job)
-addFirebaseJob(data)
-db.child("users").child("MRDnuEa2Eq_oh_Uwk3Z").update({"name": "Sean Touchstone Jr"})
-#def removeFirebaseJob(job):
 
